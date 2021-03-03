06a13fa1814a1fa19cebb6ef05bb49e27b8793fe
lcl-kawabe <kawabe@lclco.com>
2021-03-03 12:26:38 +0900

    fishにyvmの設定を追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,6 @@ status --is-interactive; and source (nodenv init -|psub)
 # rbenvの設定
 status --is-interactive; and source (rbenv init -|psub)
 
+
+set -x YVM_DIR /Users/yusuke/.yvm
+[ -r $YVM_DIR/yvm.fish ]; and source $YVM_DIR/yvm.fish
