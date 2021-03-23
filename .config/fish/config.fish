e9cf8e44cc21ace54bf5b591cd0799bab2fc8755
lcl-kawabe <kawabe@lclco.com>
2021-03-23 12:15:59 +0900

    config.fishにコメント追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ status --is-interactive; and source (nodenv init -|psub)
 # rbenvの設定
 status --is-interactive; and source (rbenv init -|psub)
 
-
+# yvmの設定
 set -x YVM_DIR /Users/yusuke/.yvm
 [ -r $YVM_DIR/yvm.fish ]; and source $YVM_DIR/yvm.fish
+
