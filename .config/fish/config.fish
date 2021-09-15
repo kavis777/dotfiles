a4d58ef56ea5386fdef3086a22a28a6f13ff2d03
kavis777 <wwrd0129@gmail.com>
2021-09-15 14:20:11 +0900

    homebrewの設定方法を変更

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,5 @@
 # homebrewの設定
-set -gx HOMEBREW_PREFIX "/opt/homebrew";
-set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-set -gx HOMEBREW_SHELLENV_PREFIX "/opt/homebrew";
-set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
-set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
-set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+status --is-interactive; and source (/opt/homebrew/bin/brew shellenv|psub)
 
 # nodenvの設定
 status --is-interactive; and source (nodenv init -|psub)
